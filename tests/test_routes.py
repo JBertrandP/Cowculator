3202ff30b0b109c8169973e2a89920ecca3df499
//offline <diego1350pro@gmail.com>
2024-11-08 22:52:10 -0600

    Se agrego render dinamico con Jinja

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,1 +1,12 @@
 # Unit tests for your Flask routes 
+
+ranch_data = [('PapuRancho', 'tumami', 2), ('JuanRancho', '999.999', 2) , ('JuanRancho', '999.999', 2) ]
+
+
+for i in range(0,len(ranch_data)):
+    print(ranch_data[i][0])
+    print(ranch_data[i][1])
+
+    if len(ranch_data) < 3 and i == len(ranch_data) -1 :
+        print("add ranch")
+    
