db7d52f76bb85a7442e18561dfc8e8d02e91e45d
//offline <diego1350pro@gmail.com>
2024-11-14 11:12:31 -0600

    Se agrego AJAX

--- frontend/static/js/ScriptMyCattle.js
+++ frontend/static/js/ScriptMyCattle.js
@@ -1,3 +1,47 @@
+function add_cow() {
+    // Get data from the form inputs
+    const cowData = {
+      name: document.getElementById("name").value,
+      age: document.getElementById("age").value,
+      breed: document.getElementById("breed").value,
+      weight: document.getElementById("weight").value
+    };
+  
+    // Send an AJAX request using fetch
+    fetch('/add_cow', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cowData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      
+        const cowDisplay = document.getElementById("cowDisplay");
+
+        // Create a new cow information block
+        const newCowHTML = `
+          <div class="cow-info">
+            <h2>Added Cow:</h2>
+            <p>ID: ${data.id}</p>
+            <p>Name: ${data.name}</p>
+            <p>Age: ${data.age}</p>
+            <p>Weight: ${data.weight}</p>
+          </div>
+        `;
+      
+        // Append the new cow info to the existing content
+        cowDisplay.innerHTML += newCowHTML;
+        
+
+      document.getElementById("cowForm").reset();
+    })
+    .catch(error => console.error('Error:', error));
+  }
+
+
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
@@ -17,3 +61,7 @@ sr.reveal('.model-text', {delay:200, origin: 'top'});
 sr.reveal('.model-img', {delay:450, origin: 'top'});
 sr.reveal('.icons', {delay:500, origin: 'left'});
 sr.reveal('.scroll-down', {delay:500, origin: 'right'});
+
+
+
+
